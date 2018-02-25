ba5522f789bf2ac4fd0de34f5225eeedcb5aec02
jchrimes-school <33974300+jchrimes-school@users.noreply.github.com>
2018-02-26 10:20:23 +1100

    Added comments
    I have added a couple of comments into the lstFunctions.

--- lstFunctions.py
+++ lstFunctions.py
@@ -1,24 +1,27 @@
 """
 <InsertNameHere> 2018
 This module contains functions that will allow data to be analysed form a list.
-Version: 0.1
+Version: 0.2
 Date Created: 2018/02/23
 Date Modified: 2018/02/23
+
+Conventions functions and variables must be prefaced with the data types they contain eg. lst, str, flt, int.
+functions should be seperated by two lines.
 """
 
 
-def lstSum(lst):
+def lstSum(lst): # A function to get the sum of a list
     fltSum = float()
     for i in lst:
         fltSum = fltSum + i
     return fltSum
 
 
-def lstAvg(lst):
+def lstAvg(lst): # A function to return the average of a list. DEPENDS UPON lstAvg!!
     return float(lstSum(lst)/len(lst))
 
 
-def lstMin(lst):
+def lstMin(lst): # A function to determine the minimum value in a list
     fltMinVal = float(lst[0])
     for i in lst:
         if i < fltMinVal:
@@ -26,7 +29,7 @@ def lstMin(lst):
     return fltMinVal
 
 
-def lstMax(lst):
+def lstMax(lst): # A function to determine the maximum value in a list.
     fltMaxVal = float(lst[0])
     for i in lst:
         if i > fltMaxVal:
