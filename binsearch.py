3b1f1b26e4d9ed635ac9e9dd6cc614fb947f1bfa
jchrimes-school <33974300+jchrimes-school@users.noreply.github.com>
2018-06-14 14:48:09 +1000

    Add a binary search for people to look at

---
+++ binsearch.py
@@ -0,0 +1,15 @@
+def binSearch(lst, value):
+        # High and low set in class initialisation.
+        high = len(lst) -1 
+        low = 0
+        while (self.low <= self.high):
+            midpoint = (high + low) /2 
+            # print(midpoint)
+            if (lst[midpoint]) > value: # If value is lower than the midpoint then truncate.
+                high = midpoint - 1
+            elif (lst[midpoint] < value):
+                low = midpoint + 1
+            else:
+                print(midpoint)
+                return midpoint
+        print("Not Found")
