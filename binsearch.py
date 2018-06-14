6ca7b3e4a955ce6d53faf45865e1dfdd2051db43
jchrimes-school <33974300+jchrimes-school@users.noreply.github.com>
2018-06-14 15:00:08 +1000

    Removed more self

--- binsearch.py
+++ binsearch.py
@@ -2,7 +2,7 @@ def binSearch(lst, value):
         # High and low set in class initialisation.
         high = len(lst) -1 
         low = 0
-        while (self.low <= self.high):
+        while (low <= high):
             midpoint = (high + low) /2 
             # print(midpoint)
             if (lst[midpoint]) > value: # If value is lower than the midpoint then truncate.
